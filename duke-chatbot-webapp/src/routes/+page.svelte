a161b9a3cf85c6f7c792f7a8f9e6f4c9af1fc58d
Haran Nallasivan <haranku16@users.noreply.github.com>
2025-04-02 22:48:11 -0400

    Initial SvelteKit setup

---
+++ duke-chatbot-webapp/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    // import Duke100 from '../components/duke100.svelte';
+    // import Header from '../components/header.svelte';
+    // import Footer from '../components/footer.svelte';
+</script>
+<!-- <Duke100 /> -->
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
